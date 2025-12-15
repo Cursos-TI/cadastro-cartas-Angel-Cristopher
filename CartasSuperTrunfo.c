cc874a868ead5bd3ef1a4ffb22a2457f1b67b927
Angel Cristopher Lúcio Queiroz <129675000+Angel-Cristopher@users.noreply.github.com>
2025-12-15 12:40:51 +0000

    Segunda etapa desafio Super Trunfo: Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,12 @@ int main(){
 //Variáveis carta 1
     char estado_1, codigoCarta_1[3], nomeCidade_1[50];
     int populacao_1, pontosTuristicos_1;
-    float area_1, pib_1;
+    float area_1, pib_1, densidadePopulacional_1, pibPerCapita_1;
 
 //Variáveis carta 2
     char estado_2, codigoCarta_2[3], nomeCidade_2[50];
     int populacao_2, pontosTuristicos_2;
-    float area_2, pib_2;
+    float area_2, pib_2, densidadePopulacional_2, pibPerCapita_2;
 
 //Inserir informações carta 1
     printf("Cadastro de informações carta 1: \n"
@@ -28,6 +28,10 @@ int main(){
     printf("Insira o número de Pontos Turísticos: \n");
     scanf("%d", &pontosTuristicos_1);
 
+//Processar dados calculados carta 1
+    densidadePopulacional_1 = (float) populacao_1 / area_1;
+    pibPerCapita_1 = (float) pib_1 /  populacao_1;
+
 //Inserir informações carta 2
     printf("\nCadastro de informações carta 2: \n"
         "Insira o Estado (A-H): \n");
@@ -45,6 +49,10 @@ int main(){
     printf("Insira o número de Pontos Turísticos: \n");
     scanf("%d", &pontosTuristicos_2);
 
+//Processar dados calculados carta 2
+    densidadePopulacional_2 = (float) populacao_2 / area_2;
+    pibPerCapita_2 = (float) pib_2 /  populacao_2;
+
 //Impressão de informações no terminal carta 1
     printf("\nCarta 1: \n"
         "Estado: %c \n"
@@ -53,8 +61,10 @@ int main(){
         "População: %d \n"
         "Área: %.2f km²\n"
         "PIB: %.2f bilhões de reais \n" 
-        "Número de Pontos Turísticos: %d \n", 
-        estado_1, estado_1, codigoCarta_1, nomeCidade_1, populacao_1, area_1, pib_1, pontosTuristicos_1);
+        "Número de Pontos Turísticos: %d \n"
+        "Densidade Populacional: %.2f hab/km² \n"
+        "PIB per Capita: R$ %.2f \n",
+        estado_1, estado_1, codigoCarta_1, nomeCidade_1, populacao_1, area_1, pib_1, pontosTuristicos_1, densidadePopulacional_1, pibPerCapita_1);
 
 //Impressão de informações no terminal carta 2
     printf("\nCarta 2: \n"
@@ -64,8 +74,10 @@ int main(){
         "População: %d \n"
         "Área: %.2f km²\n"
         "PIB: %.2f bilhões de reais \n" 
-        "Número de Pontos Turísticos: %d \n", 
-        estado_2, estado_2, codigoCarta_2, nomeCidade_2, populacao_2, area_2, pib_2, pontosTuristicos_2);
+        "Número de Pontos Turísticos: %d \n"
+        "Densidade Populacional: %.2f hab/km² \n"
+        "PIB per Capita: R$ %.2f \n",
+        estado_2, estado_2, codigoCarta_2, nomeCidade_2, populacao_2, area_2, pib_2, pontosTuristicos_2, densidadePopulacional_2, pibPerCapita_2);
 
 return 0;
 
